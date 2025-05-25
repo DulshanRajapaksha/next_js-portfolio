68e3a0277ae5d2b37fd2fa6490f6000ecf5f7ad5
Dulshan Rajapaksha <129612778+DulshanRajapaksha@users.noreply.github.com>
2025-05-25 22:23:56 +0530

    style nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,26 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 export default function Home() {
   return (
     <div>
-      <main className="font-poppins bg-yellow-50 min-h-screen mx-10 ">
+      <main className="font-poppins bg-yellow-50 min-h-screen px-10 ">
         {/* nav and hero */}
         <section>
           {/* nav */}
-          <h1>Developed By Dulshan-R</h1>
-          <ul>
-            {/* icons */}
-            <li>
-              <BsFillMoonStarsFill />
-            </li>
-            <li>
-              <a href="#">Resume</a>
-            </li>
-          </ul>
+          <nav className="py-10 flex justify-between">
+            <h1 className="text-sm">Developed By Dulshan-R</h1>
+            <ul className="flex items-center gap-5">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl mx-5" />
+              </li>
+              <li>
+                <a href="#" 
+                download={"resume.pdf"}
+                className="bg-yellow-400 text-black px-4 py-2 rounded-md hover:bg-yellow-300 transition duration-300"
+                >
+                Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </div>
