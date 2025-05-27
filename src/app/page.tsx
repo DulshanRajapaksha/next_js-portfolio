9b020d2a03cb5945ccf40c75af15c1d12b01d104
Dulshan Rajapaksha <129612778+DulshanRajapaksha@users.noreply.github.com>
2025-05-28 01:29:15 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+
+// import Reacticons
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
+// import images
+import profileImage from "/p2.png";
 
 export default function Home() {
   return (
@@ -24,6 +32,37 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+
+          {/* hero */}
+          <div className="text-center p-10">
+            <h1 className="text-5xl font-bold text-yellow-400">I'am Navod Dulshan</h1>
+            <h3 className=" text-2xl py-4">Mobile App and Website Development</h3>
+            <p className="text-sm py-4 leading-6 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est architecto. Enim molestiae necessitatibus modi totam minus expedita iusto minima tenetur impedit aspernatur doloremque dolorum officia et magnam, rerum repellat.
+            </p>
+          </div>
+          {/*socail media icon */}
+          <div className="text-5xl flex justify-center gap-16 text-gray-600">
+            <a href="">
+              <FaGithub />
+            </a>
+            <a href="">
+              <FaLinkedin />
+            </a>
+            <a href="">
+              <FaInstagramSquare />
+            </a>
+          </div>
+
+          {/* profile */}
+          <div className="relative m-auto bg-gradient-to-b from-yellow-200 w-80 h-80 p-5 mt-20 rounded-full">
+            <Image
+              src="/p2.png"
+              alt="Profile Image"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </section>
       </main>
     </div>
